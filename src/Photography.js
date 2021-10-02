e5e3419270c5f6985ca9aa1099fbcbeae4ec2c03
Schow94 <Schow94@gmail.com>
2021-10-01 22:03:12 -0700

    make more responsive

--- src/Photography.js
+++ src/Photography.js
@@ -5,28 +5,52 @@ import "./Photography.css";
 
 const images = [
 	{
-		url: "https://images.unsplash.com/photo-1632952745637-a104d96d8405?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "airplane",
+		url: "./amsterdam.jpg",
+		alt: "amsterdam",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1632235623196-cf0a9d0bf0ac?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "fall",
+		url: "./arch.jpg",
+		alt: "arch",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1607858066933-1e2e91382182?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Mnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "window",
+		url: "./bldg.jpg",
+		alt: "bldg",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1632971306870-9f54e5706994?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2M3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "aerial",
+		url: "./cat.jpg",
+		alt: "cat",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1633049919683-672362346394?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "car",
+		url: "./goldenGate.jpg",
+		alt: "goldenGate",
 	},
 	{
-		url: "https://images.unsplash.com/photo-1632952745637-a104d96d8405?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-		alt: "airplane",
+		url: "./versailles.jpg",
+		alt: "versailles",
+	},
+	{
+		url: "./goldenGate2.jpg",
+		alt: "goldenGate2",
+	},
+	{
+		url: "./versailles2.jpg",
+		alt: "versailles2",
+	},
+	{
+		url: "./waikiki.jpg",
+		alt: "waikiki",
+	},
+	{
+		url: "./yosemite.jpg",
+		alt: "yosemite",
+	},
+	{
+		url: "./zion2.jpg",
+		alt: "zion2",
+	},
+	{
+		url: "./zion.jpg",
+		alt: "zion",
 	},
 ];
 
